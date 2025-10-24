96cc566240497229d1d37fd02e4f7b4e1775872f
yangmingxin <1355628444@qq.com>
2025-10-24 16:31:54 +0800

    Fix JavaScript loading issue: use relative paths and improve Vercel config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "bundle.js",
-      publicPath: "/",
+      publicPath: "./",
       clean: true, // 清理输出目录
     },
     devServer: {

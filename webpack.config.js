0ecc487b2e798030a1a8d4fa2e900ccea48e454b
yangmingxin <1355628444@qq.com>
2025-10-24 16:16:05 +0800

    Complete deployment optimization: webpack, Vercel, Netlify, GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,70 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/main.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  devServer: {
-    port: 8080,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  resolve: {
-    extensions: [".js", ".vue", ".json"],
-    alias: {
-      vue$: "vue/dist/vue.esm.js",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/main.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/",
+      clean: true, // 清理输出目录
     },
-  },
+    devServer: {
+      port: 8080,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        } : false,
+      }),
+    ],
+    resolve: {
+      extensions: [".js", ".vue", ".json"],
+      alias: {
+        vue$: "vue/dist/vue.esm.js",
+      },
+    },
+    optimization: {
+      minimize: isProduction,
+    },
+  };
 };

015ee5de7cc45b17d738bb05e5ffbd6f7416c758
yangmingxin <1355628444@qq.com>
2026-01-09 13:40:24 +0800

    111

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = (env, argv) => {
       hot: true,
       compress: true,
       historyApiFallback: true,
+      // --- 新增下面这一行 ---
+      allowedHosts: 'all',
       static: {
         directory: path.join(__dirname, 'dist'),
       },
